5562f485d73938bc8aedff961d7ddc6710520b68
Abyudh Kattemane Annaiah <abyudhka@gmail.com>
2026-05-14 14:48:55 +0930

    feat(dbt): scaffold dbt-duckdb project with first staging model and tests

---
+++ dbt/fincrime/models/staging/stg_ibm_aml__transactions.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='view') }}
+
+with source as (
+    select * from {{ source('ibm_aml', 'transactions') }}
+),
+
+renamed as (
+    select
+        -- Identifiers
+        from_bank::varchar || ':' || account_from::varchar  as src_account_id,
+        to_bank::varchar   || ':' || account_to::varchar    as dst_account_id,
+
+        -- Temporal
+        timestamp                                            as txn_timestamp,
+        date_trunc('day', timestamp)                         as txn_date,
+
+        -- Amounts
+        amount_paid                                          as amount,
+        payment_currency                                     as currency,
+        payment_format                                       as payment_method,
+
+        -- Labels
+        is_laundering::boolean                               as is_laundering
+
+    from source
+)
+
+select * from renamed
